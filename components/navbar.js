38d0b347c5fcdceada7ec5932f8838db8afb9cac
tarun rawat <rawattarun1997@gmail.com>
2021-08-05 00:05:19 +0530

    conmponent contains navbar

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+  function navbar() {
+
+    return `<div id="navbar">
+              <h3><a href="searchReceipe.html">search receipe by name</a></h3>
+              <div class="options">
+              <h3><a href="dailyReceipe.html">Get receipe of the day</a></h3>
+              <h3><a href="latestReceipe.html">Show latest receipe.</a></h3> 
+              </div>
+              </div>
+              <div id="dataDiv"></div>`
+             
+             
+}
+
+export default navbar     // navbar here is function name
